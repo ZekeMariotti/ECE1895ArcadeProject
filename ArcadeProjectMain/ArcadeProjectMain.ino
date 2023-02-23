094b10e837cd591fc80b799d4fa86e0b75ed6e7c
ZekeMariotti <97479101+ZekeMariotti@users.noreply.github.com>
2023-02-23 11:29:11 -0500

    Create ArcadeProjectMain.ino

---
+++ ArcadeProjectMain/ArcadeProjectMain.ino
@@ -0,0 +1,27 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // Pins for joystick and LED
+  
+
+  // Pins for two input buttons and LEDs
+
+
+  // Pins for coin slot button and LED
+
+
+  // Pin for Start button
+
+
+  // Pin(s) for display
+
+
+  // Example Pins
+  pinMode(8, INPUT);
+  pinMode(7, OUTPUT);
+  digitalWrite(9, LOW);
+}
+
+// Main Loop
+void loop() {
+  
+}
